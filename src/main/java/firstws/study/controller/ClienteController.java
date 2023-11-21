fe1cb92535cb640415332eec69fec615fe01b6b1
devkevin <rauppke@gmail.com>
2023-11-21 19:50:21 -0300

    Study Spring

--- src/main/java/firstws/study/controller/ClienteController.java
+++ src/main/java/firstws/study/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package firstws.study.controller;
 
 import firstws.study.models.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +13,9 @@ public class ClienteController {
     public Cliente obterCliente(){
         return new Cliente(1,"Kevin Teste","012345678910");
     }
+    @GetMapping("/{id}")
+    public Cliente obterClientePorID(@PathVariable int id){
+        return new Cliente(id,"TesteID","929-234-234-11");
+    }
+
 }
