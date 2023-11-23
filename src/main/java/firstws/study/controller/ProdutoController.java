9068bc7639dee50df43068333f1547c6645154dc
devkevin <rauppke@gmail.com>
2023-11-23 15:44:52 -0300

    Study Spring

--- src/main/java/firstws/study/controller/ProdutoController.java
+++ src/main/java/firstws/study/controller/ProdutoController.java
@@ -6,14 +6,30 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping(path ="/api/produtos")
 public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
-    @PostMapping
+    @RequestMapping(method = {RequestMethod.POST,RequestMethod.PUT})
     public @ResponseBody Produto novoProduto(@Valid Produto produto){
         produtoRepository.save(produto);
         return produto;
     }
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+//    @PutMapping
+//    public Produto alterarProduto(@Valid Produto produto){
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
+
 }
